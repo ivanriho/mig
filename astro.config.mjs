609052296bc5df39e5c8b6d430f56df5e99a85d2
Ivan <ivanristichofren@icloud.com>
2026-03-16 16:04:05 +0100

    hopp

---
+++ astro.config.mjs
@@ -0,0 +1,19 @@
+import { defineConfig } from "astro/config";
+
+import icon from "astro-icon";
+import sitemap from "@astrojs/sitemap";
+
+import tailwindcss from "@tailwindcss/vite";
+
+// https://astro.build/config
+export default defineConfig({
+  site: "https://ivanriho.github.io",
+  base: "/Cv",
+  integrations: [icon(), sitemap()],
+  prefetch: {
+    defaultStrategy: "viewport",
+  },
+  vite: {
+    plugins: [tailwindcss()],
+  },
+});
