d907f8e64427460480edf841294ef73aa37aebd1
Ivan <ivanristichofren@outlook.com>
2026-03-18 23:04:39 +0100

    password protected for Willy

---
+++ src/config/security.ts
@@ -0,0 +1,7 @@
+export const ENABLE_PASSWORD_PROTECTION = true;
+
+// Change this value when protection is enabled.
+export const SITE_PASSWORD = "letmein";
+
+// Session key used in browser storage to remember unlocked state.
+export const PASSWORD_SESSION_KEY = "mig-site-password-ok";
